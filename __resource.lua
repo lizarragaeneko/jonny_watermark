8ca5f45039e7b4afae7cc4339811f5843f3937a4
lizarragaeneko <67381650+lizarragaeneko@users.noreply.github.com>
2021-05-24 09:31:14 +0200

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,10 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+ui_page 'ui/ui.html'
+files {
+  'ui/ui.html',
+  'ui/css/main.css',
+  'ui/js/main.js'
+}
+
+client_script "main.lua"
